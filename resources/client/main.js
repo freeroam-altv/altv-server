7853d8463f4a09a1ce86edc4f264dbc1fff8701b
JustCup <32245758+JustCup@users.noreply.github.com>
2023-03-09 11:37:26 +0300

    remove unnecessary files

--- resources/client/main.js
+++
@@ -1,6 +0,0 @@
-import * as alt from "alt-client";
-alt.onServer("log:Console", handleLogConsole);
-function handleLogConsole(msg) {
-    alt.log(msg);
-}
-export { handleLogConsole };
--- resources/server/main.js
+++
@@ -1,6 +0,0 @@
-import * as alt from "alt-server";
-import { newConfiguration } from "./utils/config/config";
-const err = await newConfiguration(".env");
-if (err !== null) {
-    alt.logError(err);
-}
